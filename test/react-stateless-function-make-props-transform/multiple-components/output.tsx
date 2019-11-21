d5a03687a35474bad9387bd364a203b6cfef15ff
Will Douglas <willygdouglas@gmail.com>
2019-11-21 15:46:52 -0700

    Stop getting PropTypes from React

--- test/react-stateless-function-make-props-transform/multiple-components/output.tsx
+++ test/react-stateless-function-make-props-transform/multiple-components/output.tsx
@@ -11,8 +11,8 @@ const Hey: React.FC<HeyProps> = ({ name }) => {
     return <div>hey, {name}</div>;
 };
 Hey.propTypes = {
-    name: React.PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
 };
 Hello.propTypes = {
-    message: React.PropTypes.string,
+    message: PropTypes.string,
 };
